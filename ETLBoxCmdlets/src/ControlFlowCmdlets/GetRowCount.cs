97379e527f8ff27b275e06929fe25e22d2fc7055
Andreas Lennartz <andreas.lennartz@akelius.de>
2019-01-28 11:06:58 +0100

    Adding first draft for powershell cmdlet

---
+++ ETLBoxCmdlets/src/ControlFlowCmdlets/GetRowCount.cs
@@ -0,0 +1,38 @@
+﻿using ALE.ETLBox.ConnectionManager;
+using ALE.ETLBox.ControlFlow;
+using System;
+using System.Management.Automation;
+
+namespace ALE.ETLBoxCmdlets.ControlFlowCmdlets {
+    [Cmdlet(VerbsCommon.Get,"RowCount")]
+    [OutputType(typeof(int?))]
+    public class GetRowCount : PSCmdlet {
+        [Parameter]
+        public string TableName { get; set; }
+        [Parameter]
+        public string Condition { get; set; }
+        //[Parameter]
+        //public RowCountOptions RowCountOptions { get; set; } 
+
+        protected override void BeginProcessing() {
+            base.BeginProcessing();
+        }
+
+
+        protected override void ProcessRecord() {
+            base.ProcessRecord();
+        }
+
+        protected override void EndProcessing() {
+            ControlFlow.CurrentDbConnection = new SqlConnectionManager(new ETLBox.ConnectionString("Data Source=.;Integrated Security=SSPI;Initial Catalog=ETLBox;"));
+            int? result = RowCountTask.Count(TableName, Condition);//, RowCountOptions);
+            WriteObject(result);
+            base.EndProcessing();
+        }
+
+        protected override void StopProcessing() {
+            base.StopProcessing();
+        }
+
+    }
+}
