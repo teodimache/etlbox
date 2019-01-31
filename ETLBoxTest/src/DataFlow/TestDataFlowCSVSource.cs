af0b836b9a361022fdba357c9b4cd4f5ec194919
Andreas Lennartz <andreas.lennartz@akelius.de>
2019-01-31 07:58:02 +0100

    Adding tests for Issue/PR #4

--- ETLBoxTest/src/DataFlow/TestDataFlowCSVSource.cs
+++ ETLBoxTest/src/DataFlow/TestDataFlowCSVSource.cs
@@ -23,25 +23,57 @@ namespace ALE.ETLBoxTest {
         public void TestInit() {
             CleanUpSchemaTask.CleanUp("test");
         }
+        
         /*
          * CSVSource (out: string[]) -> DBDestination (in: string[])
          * Table without key columns
          */
         [TestMethod]
         public void CSV_DB() {
-            TableDefinition stagingTable = new TableDefinition("test.Staging", new List<TableColumn>() {
+            List<TableColumn> columns = new List<TableColumn>() {
                 new TableColumn("Col1", "nvarchar(100)", allowNulls: false),
                 new TableColumn("Col2", "int", allowNulls: true)
-            });
+            };
+            Load_CSV_DB(columns);
+        }
+
+        /*
+         * CSVSource (out: string[]) -> DBDestination (in: string[])
+         * Table without key columns, number of columns do not match
+         * (there are more columns in Datbase than in CSV file)
+         */
+        [TestMethod]
+        public void CSV_DB_MoreColumnsInDB() {
+            List<TableColumn> columns = new List<TableColumn>() {
+                new TableColumn("Col1", "nvarchar(100)", allowNulls: false),
+                new TableColumn("Col2", "int", allowNulls: false),
+                new TableColumn("Col3", "nvarchar(100)", allowNulls: true)
+            };
+            Load_CSV_DB(columns);
+        }
+
+        /*
+         * CSVSource (out: string[]) -> DBDestination (in: string[])
+         * Table without key columns, number of columns do not match
+         * (there are more columns in CSV than in Database)
+         */
+        [TestMethod]
+        public void CSV_DB_MoreColumnsInCSV() {
+            List<TableColumn> columns = new List<TableColumn>() {
+                new TableColumn("Col1", "nvarchar(100)", allowNulls: false)
+            };
+            Load_CSV_DB(columns);
+        }
+
+        private static void Load_CSV_DB(List<TableColumn> columnsInStageTable) {
+            TableDefinition stagingTable = new TableDefinition("test.Staging", columnsInStageTable);
             stagingTable.CreateTable();
             CSVSource source = new CSVSource("src/DataFlow/Simple_CSV2DB.csv");
             DBDestination<string[]> dest = new DBDestination<string[]>() { DestinationTableDefinition = stagingTable };
             source.LinkTo(dest);
-
             source.Execute();
-            dest.Wait(); 
-
-            Assert.AreEqual(3, RowCountTask.Count("test.Staging","Col1 Like '%ValueRow%' and Col2 <> 1"));
+            dest.Wait();
+            Assert.AreEqual(3, RowCountTask.Count("test.Staging", "Col1 Like '%ValueRow%'"));
         }
 
         /*
