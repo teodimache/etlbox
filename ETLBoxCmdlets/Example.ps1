9156ba12c597bfe92ba5ec185763189113a19ca6
Andreas Lennartz <andreas.lennartz@akelius.de>
2019-01-31 08:16:26 +0100

    Moved ETLBoxCmdlets into its own repo

--- ETLBoxCmdlets/Example.ps1
+++
@@ -1,4 +0,0 @@
-﻿#Works only with powershell core!!
-cd Y:\Github\roadrunnerlenny\etlbox\ETLBoxCmdlets\bin\Debug\netcoreapp2.1
-#Add-Type -Path "C:\Program Files\dotnet\sdk\NuGetFallbackFolder\microsoft.netcore.app\2.1.0\ref\netcoreapp2.1\netstandard.dll"
-Import-Module .\ETLBoxCmdlets.dll -Force
